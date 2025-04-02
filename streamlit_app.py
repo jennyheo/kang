84950026fc4795c76864e255d507b2e4ce431b8d
jennyheo <hyj3589@gmail.com>
2025-04-02 09:29:21 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -290,20 +290,6 @@ with tab3:
 			' - 마크다운')       # 이런 식으로 - 앞 뒤에 공백 추가하면, 들여쓰기도 가능함!
      st.divider()
 
-     st.markdown("*Streamlit* is **really** ***cool***.")
-     st.markdown('''
-    :red[Streamlit] :orange[can] :green[write] :blue[text] :violet[in]
-    :gray[pretty] :rainbow[colors] and :blue-background[highlight] text.''')
-     st.markdown("Here's a bouquet &mdash;\
-            :tulip::cherry_blossom::rose::hibiscus::sunflower::blossom:")
-
-     multi = '''If you end a line with two spaces,
-     a soft return is used for the next line.
-
-     Two (or more) newline characters in a row will result in a hard return.
-     '''
-     st.markdown(multi)
-     
 st.divider()
 
 #st.write("st.session_state 객체:", st.session_state)
